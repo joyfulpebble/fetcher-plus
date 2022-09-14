b8b307bd74e789256509f9c07e1c5a576cef3943
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-14 17:18:45 +0300

    [🗃] Code cleaning

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
 import { Link } from 'react-router-dom';
 
 import FileSaver from 'file-saver';
