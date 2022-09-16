6acdc4cce503333686da1ff755c31714ecc86bde
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-16 19:08:21 +0300

    [🗃] Decomposition

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import FileSaver from 'file-saver';
 import Tools from '../core/tools/Tools';
+import FileSaver from 'file-saver';
 
-import EditorWithGetContent from './components/editor-with-get-content/EditorWithGetContent';
+import setGetEditors from './components/editors/get/lib/setGetEditors';
 import StatusBar from './components/status-bar/StatusBar';
 import Inset from './components/inset/Inset';
-import Service from '../core/API/Service';
 
 //'https://jsonplaceholder.typicode.com/posts'
 function Workspace(): JSX.Element {
   const [tempErrorStorage, setTempErrorStorage] = useState<undefined | any[]>(undefined);
-  
-  const storageData: any[] = Tools.getAllStorage(sessionStorage);
-  const parsedData: any    = JSON.parse(storageData[0]);  
-  
-  let params: object = parsedData.params;
-  let url: string    = parsedData.url;
-  
+
   const [editiorContent, setEditorContent] = useState<any>('');
   
   let blob: any = null;
@@ -29,43 +22,10 @@ function Workspace(): JSX.Element {
   
   const [currentInset, setCurrentInset] = useState(0);
   const insetNames = ['data', 'headers', 'config', 'status'];
-  const insetFunctions = [Service.getContent, Service.getHeader, Service.getConfig, Service.getStatus];
-  const editors = [
-    <EditorWithGetContent 
-    url={url} 
-    params={params}
-    editorContent={setEditorContent}
-    errorStorage={setTempErrorStorage}
-    getHandlingFunc={Tools.getMethodHandling}
-    getFunction={insetFunctions[0]}
-    />,
-    <EditorWithGetContent 
-    url={url} 
-    params={params}
-    editorContent={setEditorContent}
-    errorStorage={setTempErrorStorage}
-    getHandlingFunc={Tools.getMethodHandling}
-    getFunction={insetFunctions[1]}
-    />,
-    <EditorWithGetContent 
-    url={url} 
-    params={params}
-    editorContent={setEditorContent}
-    errorStorage={setTempErrorStorage}
-    getHandlingFunc={Tools.getMethodHandling}
-    getFunction={insetFunctions[2]}
-    />,
-    <EditorWithGetContent 
-    url={url} 
-    params={params}
-    editorContent={setEditorContent}
-    errorStorage={setTempErrorStorage}
-    getHandlingFunc={Tools.getMethodHandling}
-    getFunction={insetFunctions[3]}
-    />,]
+  const editors = setGetEditors({setEditorContent, setTempErrorStorage});
 
-    useEffect(() => {
-      console.log(insetNames[currentInset]);
+  useEffect(() => {
+    console.log(insetNames[currentInset]);
   }, [currentInset])
   
 
