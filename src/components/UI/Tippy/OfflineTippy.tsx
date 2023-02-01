34e1fa6f69c115615a033171d950265d6e138611
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-02-01 21:20:22 +0300

    [🎓] Fixed paths to tippy element styles.

--- src/components/UI/Tippy/OfflineTippy.tsx
+++ src/components/UI/Tippy/OfflineTippy.tsx
@@ -6,7 +6,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/shift-away.css';
 
-import classes from '../StatusBar.module.scss';
+import classes from './Tippy.module.scss';
 
 function OfflineTippy({online}: any): JSX.Element {
   return (
--- src/components/UI/Tippy/OnlineTippy.tsx
+++ src/components/UI/Tippy/OnlineTippy.tsx
@@ -6,7 +6,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/shift-away.css';
 
-import classes from '../StatusBar.module.scss';
+import classes from './Tippy.module.scss';
 
 function OnlineTippy(): JSX.Element {
   return (
