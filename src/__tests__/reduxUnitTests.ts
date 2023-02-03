932a17c831603eb6bb705f61560641341f3be7fd
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-02-03 22:22:56 +0300

    [✨] Added the first test, basic testing of getConfigSlice for an empty value.

--- src/__tests__/reduxUnitTests.ts
+++ src/__tests__/reduxUnitTests.ts
@@ -1,4 +1,16 @@
-test('Updates a books author and title', () => {
-  getCofigSlice
-});
+import getConfigSlice from "../redux/reducers/GetConfigSlice";
 
+import { GetConfigI } from "../types/request_configs";
+
+const initState: GetConfigI = {
+  params: {},
+  url: ''
+} 
+
+describe('getConfigSlice', () => {
+  it('should return default state when passed an ampty action', () => {
+    const result = getConfigSlice(undefined, { type: '' });
+
+    expect(result).toEqual(initState)
+  })
+} );
