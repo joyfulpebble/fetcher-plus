21d0d7666f0012016d8cbefd3315948f4f490de2
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-02-05 17:55:27 +0300

    [⚙] Update `getConfigSlice` unit tests.

--- src/__tests__/redux-tests/unit/getConfigSlice.test.ts
+++ src/__tests__/redux-tests/unit/getConfigSlice.test.ts
@@ -1,7 +1,9 @@
-import getConfigSlice from "../../../redux/reducers/getConfigSlice";
+import getConfigReducer, { getConfigSlice } from "../../../redux/reducers/getConfigSlice";
 
+import { AnyAction } from "@reduxjs/toolkit";
 import { GetConfigI } from "../../../types/request_configs";
 
+const { clearConfig, updateConfig } = getConfigSlice.actions
 const initState: GetConfigI = {
   params: {},
   url: ''
@@ -9,8 +11,33 @@ const initState: GetConfigI = {
 
 describe('getConfigSlice', () => {
   it('should return default state when passed an ampty action', () => {
-    const result = getConfigSlice(undefined, { type: '' });
+    const result = getConfigReducer(undefined, { type: '' });;
 
-    expect(result).toEqual(initState)
+    expect(result).toEqual(initState);
+  })
+
+  it('should clear config state with "clearConfig" action', () => {
+    const action: AnyAction = { type: clearConfig.type };
+    const result = getConfigReducer(initState, action);
+
+    expect(result.params).toEqual({});
+    expect(result.url).toEqual('');
+  })
+
+  it('should update config state with "updateConfig" action', () => {
+    const payload: GetConfigI = {
+      url: 'https://some-url-here',
+      params: {
+        some_parameter: 'here'
+      }
+    };
+    const action: AnyAction= { type: updateConfig.type, payload: payload };
+
+    const result = getConfigReducer(initState, action);
+
+    expect(result.url).toEqual('https://some-url-here');
+    expect(result.params).toEqual({
+      some_parameter: 'here'
+    });
   })
 });
