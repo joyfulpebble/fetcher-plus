4d361efc8a545c67a45cc258e4b5d303e01510bf
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-03-29 14:49:54 +0300

    [⚙] Query error storage cleared on the fetching page.

--- src/pages/FetchPage.tsx
+++ src/pages/FetchPage.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Form from '../components/FetchForm';
 import StatusBar from '../components/layouts/statusbar/StatusBar';
 
+import { useAppDispatch } from '../hooks/redux/redux';
+import { requestErrorSlice } from '../redux/reducers/requestErrorSlice';
+
 function FetchForm() {
-  
+  const { clearErrorStorage } = requestErrorSlice.actions;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch( clearErrorStorage() )
+  }, [])
+
   return (
     <div>
       <Form/>
